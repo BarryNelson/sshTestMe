794573e403105b5231f1a5c876dfd5104389f995
wet <barry100001@gmail.com>
2013-11-01 09:45:33 -0700

    Initial Commit

---
+++ sshTestMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sshTestMe
+//
+//  Created by wet on 11/1/13.
+//  Copyright (c) 2013 Barry Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sshTestMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sshTestMe
+//
+//  Created by wet on 11/1/13.
+//  Copyright (c) 2013 Barry Nelson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
